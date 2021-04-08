8464e32d9d02281c0caeced815d559fd6dc31624
Owen Mowery <owenmowery99@gmail.com>
2021-04-08 11:27:47 -0400

    commit tree 4ed90e9231379b6373b626a45d70a2518009b023

--- app.rb
+++ app.rb
@@ -20,6 +20,9 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    @sessions = session
+    session[:foo] = 'hello'
+
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -37,6 +40,9 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
+    
+    
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
@@ -52,6 +58,7 @@ class App < Sinatra::Base
 
   get '/logout' do
     #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
